c6269e0b86ff61c0eb0b832cbb0a441b47dd231c
Judith407 <judygonzalez407@hotmail.com>
2026-04-06 23:32:26 -0500

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('./app_server/models/db');
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,7 +8,11 @@ var logger = require('morgan');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
 var handlebars = require('hbs');
+
+require('./app_api/models/db');
 
 var app = express();
 
@@ -27,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
